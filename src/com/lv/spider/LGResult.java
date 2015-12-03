99d7bf69533ccd92c7957a1c2238af20d9a8acdf
woxiaobao <574727356@qq.com>
2015-12-03 17:07:33 +0800

    546

--- src/com/lv/spider/LGResult.java
+++ src/com/lv/spider/LGResult.java
@@ -7,18 +7,25 @@ public class LGResult {
 	
 	//部门
 	private String positionName;
+	
 	//职位
 	private String positionType;
+	
 	//工作时长
 	private String workYear;
+	
 	//学历
 	private String education;
+	
 	//招聘公司
 	private String companyName;
+	
 	//公司行业
 	private String industryField;
+	
 	//公司类型
 	private String financeStage;
+	
 	//薪资
 	private String salary;
 	
