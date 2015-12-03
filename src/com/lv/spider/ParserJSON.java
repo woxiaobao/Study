d5b8b2b8f720b2699a849f9971c7e503f6a171a7
woxiaobao <574727356@qq.com>
2015-12-03 09:42:23 +0800

    03

--- src/com/lv/spider/ParserJSON.java
+++ src/com/lv/spider/ParserJSON.java
@@ -3,6 +3,7 @@ package com.lv.spider;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
@@ -11,6 +12,13 @@ public class ParserJSON {
 	
 	public static Map<String,Object> getDATA(String results){
 		Map<String, Object> data = JSON.parseObject(results, new TypeReference<Map<String, Object>>() {});
+		//效率最高的map遍历
+//		for(Iterator<Entry<String, Object>> it=data.entrySet().iterator();it.hasNext();){
+//			it.next();
+//			Entry<String, Object> e = it.next(); 
+//			Object key = e.getKey();
+//			Object val = e.getValue();
+//		}
 		
 		Iterator<String> iterator = data.keySet().iterator();
 		while(iterator.hasNext()){
